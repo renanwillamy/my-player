c0b8511ad7a83bcc1346074ae28893fa1f15d04c
Renan <renanwillamy2@gmail.com>
2019-11-12 17:44:47 -0300

    Implementation of the imageview onClickListener

--- app/src/main/java/com/renanwillamy/myplayer/MainActivity.kt
+++ app/src/main/java/com/renanwillamy/myplayer/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.renanwillamy.myplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         img_main.setImageResource(R.drawable.player_background)
+
+        img_play.setOnClickListener{}
+        img_back.setOnClickListener{}
+        img_forward.setOnClickListener{
+            Toast.makeText(this,"Forward",Toast.LENGTH_SHORT).show()
+        }
+        img_folder.setOnClickListener{
+            val intent = Intent()
+                .setType("*/*")
+                .setAction(Intent.ACTION_GET_CONTENT)
+
+            startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 111 && resultCode == RESULT_OK) {
+            val selectedFile = data?.data //The uri with the location of the file
+            Toast.makeText(this,selectedFile?.path.toString(),Toast.LENGTH_LONG).show()
+        }
     }
 }
